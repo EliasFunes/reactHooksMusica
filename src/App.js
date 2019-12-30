433ad04de65a13c1d9e7bec9218310dd7fd6a181
Elias Funes <sk8flip720@gmail.com>
2019-12-29 21:36:26 -0300

    First commit with first version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState, useEffect} from 'react';
+import Formulario from "./components/Formulario";
+import Cancion from "./components/Cancion";
+import Informacion from "./components/Informacion";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const [artista, agregarArtista] = useState('');
+    const [letra, agregarLetra] = useState([]);
+    const [info, agregarInfo] = useState({});
+
+    const consultarApiLetra = async busqueda => {
+        const {artista, cancion} = busqueda;
+        const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+        const resultado = await axios(url);
+        agregarArtista(artista);
+        agregarLetra(resultado.data.lyrics);
+    }
+
+    const consultarApiInfo = async () => {
+        if(artista) {
+            const url = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+            const resultado = await axios(url);
+            agregarInfo(resultado.data.artists[0]);
+        }
+    }
+
+    useEffect(() => {
+        consultarApiInfo();
+    }, [artista])
+
+    return (
+        <Fragment>
+            <Formulario
+                consultarApiLetra={consultarApiLetra}
+            />
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-md-6">
+                        <Informacion
+                            info={info}
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <Cancion
+                            letra={letra}
+                        />
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
+};
 
 export default App;
